a121bf49f29ae8c8031f7bf5136ffedea1f7ae3e
deshmukhmayur <deshmukhmayur@outlook.com>
2017-07-01 22:30:22 +0530

    Added a fix to clear the uploads to avoid cluttering

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var upload = require('multer')({ dest: 'uploads/' });
 
@@ -10,8 +11,27 @@ app.set('json spaces', 40);
 // to serve static files without routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+// to delete a directory
+function clrDir(dirPath) {
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  // fs.rmdirSync(dirPath);
+};
+
 // routes
 app.post('/upload', upload.single('file'), (req, res) => {
+  // clears the uploads/ directory to purge the uploaded files
+  clrDir('uploads');
+
+  // returns the filename and file size
   res.json({
     'original_name': req.file.originalname,
     'file_size': req.file.size
